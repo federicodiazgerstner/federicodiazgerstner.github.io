a8f850afd1d0bba870dc5c206223df28372083ab
Federico <federicodiazgerstner@gmail.com>
2021-02-06 10:50:50 -0300

    added hamburger menu

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const ham_menu = document.querySelector("#hamburger");
+const list = document.querySelectorAll("li");
+console.log(list);
 let menuOpen = false;
 
 ham_menu.addEventListener('click', () => {
     if (!menuOpen) {
         ham_menu.classList.add("fa-times");
         ham_menu.classList.remove("fa-bars");
+
+        list.forEach((li) => {
+            li.classList.remove("hidden");
+        })
+
         menuOpen = true;
     } else {
         ham_menu.classList.add("fa-bars");
         ham_menu.classList.remove("fa-times");
+
+        list.forEach((li) => {
+            li.classList.add("hidden");
+        })
+
         menuOpen = false;
     }
 })
